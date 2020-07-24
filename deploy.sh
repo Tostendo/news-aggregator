b5fac278676945461c718afb184065fc53b0b718
Thomas Ostendorf <thomas.ostendorf@orbitlabs.de>
2020-07-24 12:45:07 +0200

    Modify deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-if [ -z "$1" ]
-then
-  echo "Which folder do you want to deploy to GitHub Pages?"
-  exit 1
-fi
 cd ui && yarn build
-git subtree push --prefix $1 origin gh-pages
+cd .. && git add ui/build && git commit -m "Deploy to gh-pages" 
+git subtree push --prefix ui/build origin gh-pages
