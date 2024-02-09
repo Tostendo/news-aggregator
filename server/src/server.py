b2d81b83de5911418c7a08b7820d1be730be6c58
Thomas Ostendorf <thomas.ostendorf@orbitlabs.de>
2024-02-09 12:52:31 +0000

    port from env

--- server/src/server.py
+++ server/src/server.py
@@ -291,5 +291,5 @@ def get_events():
 
 if __name__ == "__main__":
     init_db()
-    run(host='localhost', port=5000)
+    run(port=int(os.environ.get('PORT', 5000)), host=os.environ.get('HOST', 'localhost'))
 
